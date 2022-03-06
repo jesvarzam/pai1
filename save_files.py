af49cc8491e999876a8e9d6e8c2692a70c412dc3
jesvarzam <jesvarzam@alum.us.es>
2022-03-06 20:22:22 +0100

    Functionality: create table html

--- save_files.py
+++ save_files.py
@@ -62,4 +62,25 @@ def digest(path):
     return file_hash.hexdigest()
 
 
+def create_table_html(headers,report, data):
+    pre_existing_template="<!DOCTYPE html>" + "<html>" + "<head>" + "<style>"
+    pre_existing_template+="table, th, td {border: 1px solid black;border-collapse: collapse;border-spacing:8px}"
+    pre_existing_template+="</style>" + "</head>"
+    pre_existing_template+="<body>" + "<strong>" + "REPORT DATE: " + report + "</strong>" 
+    pre_existing_template+="<table style='width:50%'>"
+    pre_existing_template+='<tr>'
+    for header_name in headers:
+        pre_existing_template+="<th style='background-color:#3DBBDB;width:85;color:white'>" + header_name + "</th>"
+    pre_existing_template+="</tr>"
+    for d in data:
+        sub_template="<tr style='text-align:center'>"
+        sub_template+="<td>" + str(d[0]) + "</td>"
+        sub_template+="<td>" + str(d[1]) + "</td>"
+        sub_template+="<td>" + str(d[2]) + "</td>"
+        sub_template+="<tr/>"
+        pre_existing_template+=sub_template
+    pre_existing_template+="</table>" + "</body>" + "</html>"
+    return(pre_existing_template)
+
+
 #TODO ORDENAR CLAVES DEL DICCIONARIO PARA POSTERIORMENTE REALIZAR EL DIVIDE Y VENCERAS
